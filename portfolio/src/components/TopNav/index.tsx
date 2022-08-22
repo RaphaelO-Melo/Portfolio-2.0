43ba70509453d8d0f28abf0ce4c586f931daa577
Raphael O. Melo <raphael.o.melo@outlook.com>
2022-08-22 20:11:40 -0300

    Using pages.json as database for the pages

--- portfolio/src/components/TopNav/index.tsx
+++ portfolio/src/components/TopNav/index.tsx
@@ -1,30 +1,27 @@
 import style from './TopNav.module.scss';
 import { useLocation, Link } from 'react-router-dom';
 import classNames from 'classnames';
+import { Page } from 'types/Page';
+import pagesList from 'data/pages.json';
 
 export default function TopNav(){
 
     const location = useLocation();
-
-    const routes = [
-        {label: 'Projetos', to: '/projects'},
-        {label: 'Sobre', to: '/'},
-        {label: 'Contato', to: '/contact'},
-    ];
+    const pages : Page[] = pagesList;
 
     return (
         <nav className={style.menu}>
             <ul className={style.menu__list}>
-                {routes.map((route, index) => (
+                {pages.map((page, index) => (
                     <li key={index}>
                         <Link 
                             className={classNames({
                                 [style.menu__list__item] : true,
-                                [style['menu__list__item--selected']] : route.to === location.pathname
+                                [style['menu__list__item--selected']] : page.to === location.pathname
                             })} 
-                            to={route.to}
+                            to={page.to}
                         >
-                            {route.label}
+                            {page.label}
                         </Link>
                     </li>
                 ))}
---
+++ portfolio/src/types/Page.ts
@@ -0,0 +1,3 @@
+import pages from 'data/pages.json';
+
+export type Page = typeof pages[0];
