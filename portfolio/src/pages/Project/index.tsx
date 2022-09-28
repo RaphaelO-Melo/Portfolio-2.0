a6041cdec6a91a64e224edd81b3fda560e013459
Raphael O. Melo <raphael.o.melo@outlook.com>
2022-09-28 20:43:54 -0300

    Base hover effect on link to the project

--- portfolio/src/pages/Project/index.tsx
+++ portfolio/src/pages/Project/index.tsx
@@ -1,16 +1,26 @@
 import { useParams } from 'react-router-dom';
 import { getProject } from 'common/Library'; 
 import style from './Project.module.scss';
+import { FiExternalLink } from 'react-icons/fi';
 
 export default function Project(){
 
     const {id} = useParams();
     const {name, description, image, link, institution, tools } = getProject(Number(id));
+    const styleIcon = { color: 'white'};
 
     return(
         <section className={style.card}>
             <div className={style.card__image}>
-                <img  src={image} alt={name} />
+                <a className={style.card__image__container} href={link} target='blank'>
+                    <img  src={image} alt={name} />
+                    <span>
+                        <p>
+                            Abrir projeto    
+                        </p>
+                        <FiExternalLink style={styleIcon} />
+                    </span>
+                </a>
             </div>
             <div className={style.card__content}>
                 <h2 className={style.card__content__name}>{name}</h2>
