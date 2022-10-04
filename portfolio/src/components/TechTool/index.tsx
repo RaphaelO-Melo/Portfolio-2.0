d5bbe298e65009f404c0e4d1c86efb9a0ca5d3bc
Raphael O. Melo <raphael.o.melo@outlook.com>
2022-10-04 20:37:12 -0300

    Tech Tool component style and resposivity

--- portfolio/src/components/TechTool/index.tsx
+++ portfolio/src/components/TechTool/index.tsx
@@ -4,19 +4,21 @@ import {ReactComponent as LogoCss} from 'assets/icons/css.svg';
 import {ReactComponent as LogoJavascript} from 'assets/icons/javascript.svg';
 import {ReactComponent as LogoCsharp} from 'assets/icons/c#.svg';
 
+import style from './TechTool.module.scss';
+
 interface Tool {
     key: number,
     toolId: number,
 }
 
-export default function TechTool({key, toolId} : Tool){
+export default function TechTool({toolId} : Tool){
     return(
-        <>
-            {toolId === 1 ? <LogoJava width={'70px'} height={'70px'} /> : ''}
-            {toolId === 2 ? <LogoHtml width={'70px'} height={'70px'} /> : ''}
-            {toolId === 3 ? <LogoCss width={'70px'} height={'70px'} /> : ''}
-            {toolId === 4 ? <LogoJavascript width={'70px'} height={'70px'} /> : ''}
-            {toolId === 5 ? <LogoCsharp width={'70px'} height={'70px'} /> : ''}
-        </>
+        <span className={style.icon}>
+            {toolId === 1 ? <LogoJava width={'100%'} height={'100%'} /> : ''}
+            {toolId === 2 ? <LogoHtml width={'100%'} height={'100%'} /> : ''}
+            {toolId === 3 ? <LogoCss width={'100%'} height={'100%'} /> : ''}
+            {toolId === 4 ? <LogoJavascript width={'100%'} height={'100%'} /> : ''}
+            {toolId === 5 ? <LogoCsharp width={'100%'} height={'100%'} /> : ''}
+        </span>
     );
 }
