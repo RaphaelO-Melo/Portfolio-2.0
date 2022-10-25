78703cac6b9bf00fe95a0b476470ade8423a8c4e
Raphael O. Melo <raphael.o.melo@outlook.com>
2022-10-24 21:07:43 -0300

    Two more real projects added

--- portfolio/src/components/TechTool/index.tsx
+++ portfolio/src/components/TechTool/index.tsx
@@ -5,6 +5,9 @@ import {ReactComponent as LogoJavascript} from 'assets/icons/javascript.svg';
 import {ReactComponent as LogoCsharp} from 'assets/icons/c#.svg';
 import {ReactComponent as LogoUnity} from 'assets/icons/unity.svg';
 import {ReactComponent as LogoBootstrap} from 'assets/icons/bootstrap.svg';
+import {ReactComponent as LogoReact} from 'assets/icons/react.svg';
+import {ReactComponent as LogoSass} from 'assets/icons/sass.svg';
+import {ReactComponent as LogoTypescript} from 'assets/icons/typescript.svg';
 
 import style from './TechTool.module.scss';
 
@@ -23,6 +26,9 @@ export default function TechTool({toolId} : Tool){
             {toolId === 5 ? <LogoCsharp width={'100%'} height={'100%'} /> : ''}
             {toolId === 6 ? <LogoUnity width={'100%'} height={'100%'} /> : ''}
             {toolId === 7 ? <LogoBootstrap width={'100%'} height={'100%'} /> : ''}
+            {toolId === 8 ? <LogoReact width={'100%'} height={'100%'} /> : ''}
+            {toolId === 9 ? <LogoTypescript width={'100%'} height={'100%'} /> : ''}
+            {toolId === 10 ? <LogoSass width={'100%'} height={'100%'} /> : ''}
         </span>
     );
 }
